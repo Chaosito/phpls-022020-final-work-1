f7cf24f7266fd4f87df9331d11d532d6538177c3
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-08 00:24:21 +0600

    Added composer and composer autoloader

--- config.php
+++ config.php
@@ -5,11 +5,6 @@
 mb_internal_encoding("UTF-8");  // Кодировка
 date_default_timezone_set('Asia/Almaty'); // Часовой пояс
 
-spl_autoload_register(function ($classname) {
-    $file = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR."{$classname}.php";
-    if (file_exists($file)) {
-        require_once($file);
-    }
-});
+include_once('vendor/autoload.php');
 
 $curUser = new FinalWork\CurrentUser();
