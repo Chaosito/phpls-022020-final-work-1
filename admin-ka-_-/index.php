b52a306acdd4509597bec6ff17297ce8be317d8b
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-11 12:23:22 +0600

    return getting users & orders

--- admin-ka-_-/index.php
+++ admin-ka-_-/index.php
@@ -3,4 +3,7 @@ namespace FinalWork;
 
 require_once("../config.php");
 
+$allUsers = User::getAllUsers();
+$allOrders = Order::getAllOrders();
+
 echo $twig->render('admin.twig', ['allUsers' => $allUsers, 'allOrders' => $allOrders]);
